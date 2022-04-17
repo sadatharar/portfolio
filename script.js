7941ab380a113ab2d4d24171503ae46a1f227e04
unknown <hararsedet@gmail.com>
2022-04-17 20:51:22 +0300

    myfristcommit

---
+++ script.js
@@ -0,0 +1,11 @@
+const humbergar = document.querySelector(".humbergar");
+const NavMenu = document.querySelector(".nav-menu");
+
+
+
+humbergar.addEventListener('click', ()=> {
+    humbergar.classList.toggle('active');
+    navVab.classList.toggle('active');
+})
+
+document.querySelectorAll()
